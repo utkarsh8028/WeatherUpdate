c571ea765592a7176fabe3cfbe9fb671b8adc66e
utsh <longliveutkarsh@gmail.com>
2015-07-10 23:28:38 +0530

    Weather app

---
+++ app/src/main/java/com/example/utkarsh/sunshine/sync/SunshineSyncService.java
@@ -0,0 +1,29 @@
+package com.example.utkarsh.sunshine.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by utkarsh on 26-04-2015.
+ */
+public class SunshineSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static SunshineSyncAdapter sSunshineSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        Log.d("SunshineSyncService", "onCreate - SunshineSyncService");
+        synchronized (sSyncAdapterLock) {
+            if (sSunshineSyncAdapter == null) {
+                sSunshineSyncAdapter = new SunshineSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSunshineSyncAdapter.getSyncAdapterBinder();
+    }
+}
